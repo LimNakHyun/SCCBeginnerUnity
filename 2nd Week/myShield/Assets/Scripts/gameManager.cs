886692333d7c0cf630cc6c645ef645f7f59a14ed
LimNakHyun <dlaskrgus8@gmail.com>
2023-07-04 21:21:35 +0900

    사각형 떨어지게 하기 및 시간 흐르게 하기 (4/8)

---
+++ 2nd Week/myShield/Assets/Scripts/gameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour
+{
+    public GameObject square;
+    public Text timeText;
+    float alive = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("makeSquare", 0.0f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        alive += Time.deltaTime;
+        timeText.text = alive.ToString("N2");
+    }
+
+    void makeSquare()
+    {
+        Instantiate(square);
+    }
+}
