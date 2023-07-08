dd22d5c713f63deeb2b7ae783b8c5c2cbbe61f41
LimNakHyun <dlaskrgus8@gmail.com>
2023-07-09 02:34:29 +0900

    밥 쏘기 (3/8)

---
+++ 3rd Week/dogvscat/Assets/Scripts/gameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public GameObject dog;
+    public GameObject food;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("makeFood", 0.0f, 0.2f);
+    }
+
+    void makeFood()
+    {
+        float x = dog.transform.position.x;
+        float y = dog.transform.position.y;
+        Instantiate(food, new Vector3(x, y, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
