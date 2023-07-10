c8ca592fa6cf6285f3e80a60f37454c0bd801208
LimNakHyun <dlaskrgus8@gmail.com>
2023-07-10 22:35:43 +0900

    고양이 나타내기(2) - 내려오기/충돌, 등장하기/종료 (5/8)

---
+++ 3rd Week/dogvscat/Assets/Scripts/cat.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cat : MonoBehaviour
+{
+    float full = 5.0f;
+    float energy = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float x = Random.Range(-8.5f, 8.5f);
+        float y = 30f;
+        transform.position = new Vector3(x, y, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (energy < full)
+        {
+            transform.position += new Vector3(0, -0.02f, 0);
+
+            if (transform.position.y < -16.0f)
+            {
+                // 게임오버!
+                gameManager.I.gameOver();
+            }
+        }
+        else
+        {
+            if (transform.position.x > 0)
+            {
+                transform.position += new Vector3(0.01f, 0, 0);
+            }
+            else
+            {
+                transform.position += new Vector3(-0.01f, 0, 0);
+            }
+            Destroy(gameObject, 3.0f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "food")
+        {
+            if (energy < full)
+            {
+                energy += 1.0f;
+                Destroy(collision.gameObject);
+                gameObject.transform.Find("hungry/Canvas/front").transform.localScale = new Vector3(energy / full, 1.0f, 0.0f);
+            }
+            else
+            {
+                gameObject.transform.Find("hungry").gameObject.SetActive(false);
+                gameObject.transform.Find("full").gameObject.SetActive(true);
+            }
+        }
+    }
+}
--- 3rd Week/dogvscat/Assets/Scripts/gameManager.cs
+++ 3rd Week/dogvscat/Assets/Scripts/gameManager.cs
@@ -6,10 +6,18 @@ public class gameManager : MonoBehaviour
 {
     public GameObject dog;
     public GameObject food;
+    public GameObject normalCat;
+    public GameObject retryBtn;
+    public static gameManager I;
+    void Awake()
+    {
+        I = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("makeFood", 0.0f, 0.1f);
+        InvokeRepeating("makeCat", 0.0f, 1.0f);
     }
 
     void makeFood()
@@ -19,9 +27,20 @@ public class gameManager : MonoBehaviour
         Instantiate(food, new Vector3(x, y, 0), Quaternion.identity);
     }
 
+    void makeCat()
+    {
+        Instantiate(normalCat);
+    }
+
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void gameOver()
+    {
+        retryBtn.SetActive(true);
+        Time.timeScale = 0.0f;
     }
 }
